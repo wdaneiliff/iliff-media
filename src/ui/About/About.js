edc7e334f20ce1f0d604da85b800c9a1929e138e
Dane Iliff <wdaneiliff@gmail.com>
2016-12-02 16:33:50 -0800

    Structured About Section

--- src/ui/About/About.js
+++ src/ui/About/About.js
@@ -12,6 +12,17 @@ export default class About extends Component {
         <div className="content-container">
         <div className="content">
           <h1>About Us</h1>
+          <p>Mustache wayfarers cred kitsch, quinoa bicycle rights cliche
+            dreamcatcher ennui snackwave. Heirloom umami shabby chic squid,
+            tumeric intelligentsia flexitarian fashion axe copper mug lo-fi
+            activated charcoal vice. Selvage man braid air plant, succulents
+            health goth tumeric cold-pressed bespoke humblebrag shabby chic
+            austin tilde thundercats church-key freegan. Authentic polaroid
+            synth, tumblr tilde retro iPhone. Church-key woke +1 godard, VHS
+            prism keffiyeh tattooed quinoa art party shabby chic snackwave man
+            bun. Heirloom humblebrag sustainable, woke kale chips sartorial
+            microdosing venmo fingerstache air plant copper mug. Brooklyn umami
+             biodiesel hella lo-fi.</p>
         </div>
       </div>
     </section>
