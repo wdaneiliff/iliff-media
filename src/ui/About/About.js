30abba910b2834d1d79c7419e6d355178dd2d7d1
Dane Iliff <wdaneiliff@gmail.com>
2017-01-02 00:13:22 -0800

    Added Portrait

--- src/ui/About/About.js
+++ src/ui/About/About.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
 import styles from './About.css';
-import logo from './about-guy.png';
+import portrait from './portrait-cartoon.png';
+import aboutGuy from './portrait-cartoon.png';
 
 export default class About extends Component {
   render() {
     return (
-      <div>
       <section ref="about-section" className="about-section-container">
 
         <div className="content-container">
           <h1>About</h1>
         <div className="content">
-          <p>Iliff Media is a creative design and development agency
-             specializing in website design, development, logo creation, character
-             illustration, web animation, HTML5 banner ads. Let's work together to build
+          <p>Iliff Media is a creative design and development agency created by Dane Iliff.
+             It specializes in website design, development, logo creation, character
+             illustration, web animation, HTML5 banner ads and more. Let's work together to build
              you a personal and creative digital experience.
          </p>
         </div>
       </div>
+      <div className="about-pic-container">
+
+        <img src={portrait}/>
+
+
+      </div>
     </section>
-    <div className="about-pic-container">
-      <img src={logo}/>
-    </div>
-  </div>
     );
   }
 }
